ffdbbca24cd11b1c4885d3f9a4306ccb33b90ecc
krunpy <maksimglinin17984@gmail.com>
2025-08-20 00:30:05 +0500

    sign up and login template

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./components/App/App.jsx";
+import { Login } from "./components/Login/Login.jsx";
+import { SignUp } from "./components/SignUp/SignUp.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
   },
 ]);
 createRoot(document.getElementById("root")).render(
