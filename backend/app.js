dc626664519cb0c3fe89213e38203fd3b033dd79
krunpy <maksimglinin17984@gmail.com>
2025-09-20 17:10:17 +0500

    Improve socket.io integration with user namespace, add profile update endpoint with error handling, and standardize API responses. Update frontend configuration for better development experience and replace TestLogin component with TestChatComponent.

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,13 @@
 const express = require("express");
-const expressWs = require("express-ws");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const passport = require("./passport");
 const prisma = require("./prisma");
+const { instrument } = require('@socket.io/admin-ui')
 //require("dotenv").config();
-const io = require("socket.io")(8080, {
-  cors: {
-    origin: ["http://localhost:5173"],
-  },
-});
+
 
 const app = express();
-expressWs(app);
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
@@ -30,13 +25,46 @@ app.get("/", (req, res) => {
   console.log(req.cookies);
 });
 
-io.on("connection", (socket) => {
-  console.log(socket.id);
+
+const io = require("socket.io")(8080, {
+  cors: {
+    origin: ["http://localhost:5173", "https://admin.socket.io"],
+  },
 });
 
-io.on("send-message", (message) => {
-  console.log(message);
-});
+
+const userIo = io.of('/user')
+userIo.on('connection', socket => {
+  console.log('Connected to userspace' + socket.username)
+})
+
+userIo.use((socket, next) => {
+  if (socket.handshake.token) {
+    socket.username = socket.handshake.token
+    next()
+  } else {
+    next(new Error('No token'))
+  }
+})
+
+io.on("connection", socket => {
+  console.log(socket.id)
+  socket.on('send-message', (room, message) => {
+    console.log(message)
+    if (!room) {
+      socket.broadcast.emit('recieve-message', message)
+    } else {
+      console.log(room)
+      socket.to(room).emit('recieve-message', message)
+    }
+  })
+  socket.on('join-room', (room, cb) => {
+    socket.join(room)
+    cb("Joined room")
+  })
+})
+
+instrument(io, {auth: false})
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running at http://localhost:${process.env.PORT}`);
