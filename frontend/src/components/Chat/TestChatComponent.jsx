097e7421d18e36c84e6e4cb8d7fdae6ad1f191c9
krunpy <maksimglinin17984@gmail.com>
2025-09-22 13:19:23 +0500

    Refactor chat retrieval and message handling: Simplify chat member inclusion in the backend, enhance pagination logic, and update frontend to manage chat state and create new chats when necessary.

--- frontend/src/components/Chat/TestChatComponent.jsx
+++ frontend/src/components/Chat/TestChatComponent.jsx
@@ -3,21 +3,51 @@ import { socket } from "../../socket";
 import { useParams } from "react-router-dom";
 
 export default function ChatComponent() {
-  // 1: check if chat exists
-  // 2: if it does, get the messages
-  // 3: if it doesn't, set chat to null, create chat when user clicks send button
-  // 4: get the messages
-  // 5: display the messages
-  // 6: send a message
-  // 7: display the message
-  // const [chat, setChat] = useState(null);
+  // 1: get chat info
+  // 2: if there's no chat, set chat to null, create chat when user clicks send button
+  // 3: get the messages
+  // 4: display the messages
+  // 5: send a message
+  // 6: display the message
+  const [chat, setChat] = useState(null);
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
   // const [input, setInput] = useState("");
   // const [room, setRoom] = useState("");
-
+  const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
   const { room } = useParams();
   console.log(room);
+
+  async function getChatInfo() {
+    try {
+      const result = await fetch(`${API_BASE_URL}/api/chats/${room}`, {
+        credentials: "include"
+      });
+      const response = await result.json();
+      if (!response) return setChat(null)
+        setChat(response)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getChatInfo()
+  }, [room])
+
+  async function createChat() {
+    try {
+      const result = await fetch(`${API_BASE_URL}/api/chats`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify({ type: "private", membersId: [room], name: room })
+      });
+      const response = await result.json();
+      setChat(response)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   function displayMessage(message) {
     setMessages((prev) => [...prev, message]);
@@ -34,10 +64,16 @@ export default function ChatComponent() {
       setMessages((prev) => [...prev, message]);
     });
   }, [room]);
-  // const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  
   async function sendMessage(message) {
+    if (!chat) {
+      createChat()
+    }
     socket.emit("send-message", room, message);
   }
+
+  
+
 
   /* async function getChat() {
     const res = await fetch(`${API_BASE_URL}/api/chat/${room}`, {
