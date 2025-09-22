0326560ae0a0c0c93ccf5a89340629f02fedd65d
krunpy <maksimglinin17984@gmail.com>
2025-09-22 12:39:03 +0500

    move socket to its own file.

--- frontend/src/components/Chat/TestChatComponent.jsx
+++ frontend/src/components/Chat/TestChatComponent.jsx
@@ -1,39 +1,45 @@
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
+import { socket } from "../../socket";
 import { useParams } from "react-router-dom";
 
 export default function ChatComponent() {
- // const [chat, setChat] = useState(null);
+  // 1: check if chat exists
+  // 2: if it does, get the messages
+  // 3: if it doesn't, set chat to null, create chat when user clicks send button
+  // 4: get the messages
+  // 5: display the messages
+  // 6: send a message
+  // 7: display the message
+  // const [chat, setChat] = useState(null);
   const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState("")
-  const [input, setInput] = useState("");
+  const [message, setMessage] = useState("");
+  // const [input, setInput] = useState("");
   // const [room, setRoom] = useState("");
-  const socket = io("http://localhost:8080"); // Подключение к серверу
-  const userSocket = io("http://localhost:8080/user", {auth: {token: 'test'}});
+
   const { room } = useParams();
-  console.log(room)
+  console.log(room);
+
   function displayMessage(message) {
-    setMessages(prev => [...prev, message])
+    setMessages((prev) => [...prev, message]);
   }
   useEffect(() => {
-    socket.on('connect', () => {
-      setMessages(messages => [...messages, "Connected"])
-      socket.emit('join-room', room, message =>{ displayMessage(message)})
-    })
+    socket.on("connect", () => {
+      setMessages((messages) => [...messages, "Connected"]);
+      socket.emit("join-room", room, (message) => {
+        displayMessage(message);
+      });
+    });
 
-    socket.on('recieve-message', (message) => {
-      setMessages(prev => [...prev, message])
-    })
-  }, [room])
+    socket.on("recieve-message", (message) => {
+      setMessages((prev) => [...prev, message]);
+    });
+  }, [room]);
   // const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
   async function sendMessage(message) {
-    socket.emit('send-message', room, message)
-    
+    socket.emit("send-message", room, message);
   }
-  
-  
 
- /* async function getChat() {
+  /* async function getChat() {
     const res = await fetch(`${API_BASE_URL}/api/chat/${room}`, {
       credentials: "include",
     });
@@ -52,13 +58,20 @@ export default function ChatComponent() {
         <h1>Chat</h1>
         <div>
           <div>
-            {messages.map(message => (
+            {messages.map((message) => (
               <div>
                 <p>{message}</p>
               </div>
             ))}
           </div>
-          <input type="text" name="message-input" id="message-input" placeholder="type something here..."  value={message} onChange={(e) => setMessage(e.target.value)}/>
+          <input
+            type="text"
+            name="message-input"
+            id="message-input"
+            placeholder="type something here..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
           <button onClick={() => sendMessage(message)}>Send</button>
         </div>
       </div>
